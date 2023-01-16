a9d8234dd7e9a00d3b641d0c033d607872aef615
VasaOfficial <VasaPofficial@gmail.com>
2023-01-16 23:23:34 +0100

    Home page setup and add images

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/HomePage/Homepage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Route exact path="/" components={Home}></Route>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/HomePage/Homepage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./homapege.css";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="card">
+        <h3>Card 1</h3>
+        <p>Card discription</p>
+      </div>
+      <div className="card">
+        <h3>Card 2</h3>
+        <p>Card discription 2</p>
+      </div>
+      <div className="card">
+        <h3>Card 3</h3>
+        <p>Card discription 3</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
